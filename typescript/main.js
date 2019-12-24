4133eff99433ea485d3cf8c9c3166c662409b61d
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-12-23 23:12:38 -0300

    realizando exercicios

---
+++ typescript/main.js
@@ -0,0 +1,9 @@
+var minhaVar = 'minha variável';
+function minhaFunction(x, y) {
+    return x + y;
+}
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
---
+++ typescript/main.ts
@@ -0,0 +1,12 @@
+var minhaVar = 'minha variável';
+
+function minhaFunctionTs(x, y) {
+    return x + y;
+}
+
+var numeros = [1,2,3];
+numeros.map(function (valor) {
+    return valor * 2;
+})
+
+numeros.map( valor => valor * 2);
