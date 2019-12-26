577d90cd7b1d394daa00b9310745f2b63d3d69ba
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-12-25 23:17:41 -0300

    criando rotas e componentes

--- projeto-angular/src/app/cursos/cursos.component.ts
+++ projeto-angular/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -9,10 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java', 'Angular', 'Javascript', 'SQL'];
+  cursos: string[] ;
 
-  constructor() {
+  constructor(private service: CursosService) {
     this.nomePortal = 'http://loraine.training';
+    this.cursos = this.service.getCursos();
   }
 
   ngOnInit() {
