1155f4b22ddf0de455d388de233c7b316d9bcf3f
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-12-26 23:03:39 -0300

    criando componentes de restaurant-detail

---
+++ meat-app-starter/src/app/restaurants/restaurant/restaurants.service.ts
@@ -0,0 +1,30 @@
+import { Restaurant } from './restaurant.model';
+import { Http } from '@angular/http';
+import { MEAT_API } from 'app/app.api';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { Injectable } from '@angular/core';
+import { ErrorHandler } from 'app/app.error-handler';
+
+@Injectable()
+export class RestaurantsService {
+
+  constructor(private http: Http){}
+
+  // Método que retorna uma lista de restaurantes
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handleError);
+  }
+
+  // Método que recebe um id por parâmetro e retorna um restaurante
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handleError);
+  }
+}
