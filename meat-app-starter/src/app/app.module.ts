ad336a3adca7c6f541ac94c2cc16646f8da4863d
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-01-06 21:12:51 -0300

    Finalizando a parte de Módulos

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadingStrategy, PreloadAllModules } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
@@ -11,15 +11,13 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurant/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -40,15 +38,11 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule, //FormsModule e ReactiveFormsModule são importados juntos
-    RouterModule.forRoot(ROUTES)
+    SharedModule.forRoot(), //FormsModule e ReactiveFormsModule são importados juntos
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules}) //pré-carregamento de todos os módules
   ],
 
   providers: [
-    RestaurantsService,
-    OrderService,
-    ShoppingCartService,
-    FormsModule,
     {
       provide: LOCALE_ID, useValue: 'pt-BR'
     }],
