ff0b9dede71f986a3f7fccaed2df2663af484cca
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-01-07 22:10:40 -0300

    Adicionando animações em Reviews e finalizando a página not found

--- meat-app-starter/src/app/restaurant-detail/reviews/reviews.component.ts
+++ meat-app-starter/src/app/restaurant-detail/reviews/reviews.component.ts
@@ -2,12 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { RestaurantsService } from 'app/restaurants/restaurant/restaurants.service';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-reviews',
-  templateUrl: './reviews.component.html'
+  templateUrl: './reviews.component.html',
+  animations: [
+    trigger('reviewsAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms ease-in')
+      ])
+    ])
+  ]
 })
 export class ReviewsComponent implements OnInit {
+
+  reviewsState = 'ready';
 
   reviews: Observable<any>;
 
