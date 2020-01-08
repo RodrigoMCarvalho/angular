43ddb445f31ffe875f74cb3041baf58bd6f387b9
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-01-07 21:52:59 -0300

    Realizando mais animações

--- meat-app-starter/src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ meat-app-starter/src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MenuItem } from './menu-item.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
   // Sempre que tiver uma proprietade que o elemento parent for informar, deverá anotar com @Input()
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter;
+
+  menuState = 'ready';
 
   constructor() { }
 
