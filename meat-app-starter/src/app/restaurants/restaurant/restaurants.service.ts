dba6e9746411b6a6b1ce7c36c7b7bed0878c7da4
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-01-10 23:09:42 -0300

    Refatorando a aplicação para a versão 4.3

--- meat-app-starter/src/app/restaurants/restaurant/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurant/restaurants.service.ts
@@ -1,44 +1,46 @@
 import { Restaurant } from './restaurant.model';
-import { Http } from '@angular/http';
+
 import { MEAT_API } from 'app/app.api';
-import { Observable } from 'rxjs/Observable';
+
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-import { Injectable } from '@angular/core';
 import { ErrorHandler } from 'app/app.error-handler';
 import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RestaurantsService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   // Método que retorna uma lista de restaurantes
   restaurants(search?: string): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`, {params: { q: search }}) // q = busca genérica em todos os atributos
-          .map(response => response.json())                                  // não somente no nome
-          .catch(ErrorHandler.handleError);
+    let params: HttpParams = undefined;
+    if(params) {
+      params = new HttpParams().set('q', search);   // q = busca genérica em todos os atributos
+    }
+    return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`, {params:  params })
+
   }
 
   // Método que recebe um id por parâmetro e retorna um restaurante
   restaurantById(id: string): Observable<Restaurant> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}`)
-          .map(response => response.json())
-          .catch(ErrorHandler.handleError);
+    return this.http.get<Restaurant>(`${MEAT_API}/restaurants/${id}`)
+
   }
 
   // Método que recebe o id do restaurante por parâmetro e retorna o reviews associado
   reviewsOfRestaurants(id: string): Observable<any> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
-          .map(response => response.json())
-          .catch(ErrorHandler.handleError);
+    return this.http.get<Restaurant>(`${MEAT_API}/restaurants/${id}/reviews`)
+
   }
 
   menuOfRestaurants(id: string): Observable<MenuItem> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
-          .map(response => response.json())
-          .catch(ErrorHandler.handleError);
+    return this.http.get<MenuItem>(`${MEAT_API}/restaurants/${id}/menu`)
+
   }
 }
--- meat-app-starter/src/app/restaurants/restaurants.component.ts
+++ meat-app-starter/src/app/restaurants/restaurants.component.ts
@@ -4,11 +4,13 @@ import { RestaurantsService } from './restaurant/restaurants.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
+import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/from';
+
 import { Observable } from 'rxjs/Observable';
 
 @Component({
