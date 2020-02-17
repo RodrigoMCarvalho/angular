160c19098b01e0629580749c6528d06dae4439f5
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-02-16 21:42:06 -0300

    Upload Arquivo: Barra de Progresso + Observando Eventos Http

--- Curso-Angular-Loiane/requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ Curso-Angular-Loiane/requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from 'src/app/service/upload-file.service';
 import { environment } from 'src/environments/environment';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-upload-file',
-  templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.scss']
+  templateUrl: './upload-file.component.html'
 })
 export class UploadFileComponent implements OnInit {
 
   files: Set<File>;
-
+  progress = 0;
 
   constructor(private uploadService: UploadFileService) { }
 
@@ -28,14 +28,26 @@ export class UploadFileComponent implements OnInit {
       fileNames.push(selectedFiles[i].name);    //mostra os nomes dos arquivos no input
       this.files.add(selectedFiles[i]);         //adiciona no files para enviar para o servidor node
     }
-    const teste = document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+    document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+
+    this.progress = 0;
   }
 
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.uploadService.upload(this.files, `${environment.BASE_URL}/upload`)
-          .subscribe(res => console.log('Upload concluído'))
+          .subscribe((event: HttpEvent<Object>) => {
+            console.log(event);
+            if (event.type === HttpEventType.Response) {
+              console.log('Upload concluído');
+            }
+            else if (event.type === HttpEventType.UploadProgress) {
+              const percentDone = Math.round((event.loaded * 100) / event.total);
+              console.log('Progresso: ', percentDone);
+              this.progress = percentDone;
+            }
+          });
     }
-  }
 
+  }
 }
