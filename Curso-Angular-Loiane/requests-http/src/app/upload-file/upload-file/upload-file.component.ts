95e47fe489c4b5e1d52d6678432f0850c177d396
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-02-16 21:57:14 -0300

    Http: Criando operador RxJS customizado

--- Curso-Angular-Loiane/requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ Curso-Angular-Loiane/requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from 'src/app/service/upload-file.service';
 import { environment } from 'src/environments/environment';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs.operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -36,17 +37,25 @@ export class UploadFileComponent implements OnInit {
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.uploadService.upload(this.files, `${environment.BASE_URL}/upload`)
-          .subscribe((event: HttpEvent<Object>) => {
-            console.log(event);
-            if (event.type === HttpEventType.Response) {
-              console.log('Upload concluído');
-            }
-            else if (event.type === HttpEventType.UploadProgress) {
-              const percentDone = Math.round((event.loaded * 100) / event.total);
-              console.log('Progresso: ', percentDone);
-              this.progress = percentDone;
-            }
-          });
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+          .subscribe(res => console.log('Upload concluído'));
+        /*.subscribe((event: HttpEvent<Object>) => {
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log('Upload concluído');
+          }
+          else if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round((event.loaded * 100) / event.total);
+            console.log('Progresso: ', percentDone);
+            this.progress = percentDone;
+          }
+        });*/
     }
 
   }
