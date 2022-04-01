50c8005142369d8953a800745e2305bc6045d890
Rodrigo <rodrigo_moreira84@hotmail.com>
2022-04-01 19:43:39 -0300

    tratamento de erros

--- crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Course } from './../model/course';
 import { CoursesService } from './../services/courses.service';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -13,8 +15,23 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns: string[] = ['name', 'category'];
 
-  constructor(private courseService: CoursesService) {
-    this.courses$ = this.courseService.list();
+  constructor(
+        private courseService: CoursesService,
+        private dialog: MatDialog
+      ) {
+    this.courses$ = this.courseService.list()
+    .pipe(
+      catchError(error => {
+        this.onError("Erro para carregar os cursos. Mensagem: " + error.statusText)
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
